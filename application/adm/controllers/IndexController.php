1f032da7e01ee54c37ee91fcff24cb1b78fb6e9d
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2010-12-08 17:00:35 +0100

    Small changes

--- application/adm/controllers/IndexController.php
+++ application/adm/controllers/IndexController.php
@@ -2,8 +2,6 @@
 
 
 /**
- * Index controller.
- * @version $Id: IndexController.php 18775 2010-09-15 07:54:09Z akm $
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2010, w3concepts AG
  */
@@ -11,12 +9,15 @@
 class IndexController extends Zend_Controller_Action {
 
 	public function init() {
-		/* Initialize action controller here */
+		
+		if ($this->getRequest()->getParam('ajax')) {
+			$this->_helper->layout->disableLayout();
+		}
 	}
 
 	public function indexAction() {
 	
-		// $this->_redirect('data');	
+		
 	}
 	
 	public function managementAction() {
