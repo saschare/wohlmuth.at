d6a8a995cf55be782fd9f3a2561afba50e8f6d01
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2010-12-28 14:38:03 +0100

    Implmented UUID generator plugin.

--- application/plugins/generic/utilities/uuid/Class.php
+++ application/plugins/generic/utilities/uuid/Class.php
@@ -4,14 +4,18 @@
 /**
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2010, w3concepts AG
- * 
- * {@id $Id: Class.php 18915 2010-09-21 10:41:39Z akm $}
  */
 
 class UuidPluginController extends Aitsu_Adm_Plugin_Controller {
+	
+	public function init() {
+		
+		$this->_helper->layout->disableLayout();
+	}
 
 	public function indexAction() {
 
+		header("Content-type: text/javascript");
 	}
 
 }
