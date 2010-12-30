cb33595d9c2d6b409cd9a8ff3fb9104473feba8a
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2010-12-30 10:27:39 +0100

    Cleaning up things a bit.

--- application/plugins/generic/data/freehand/Class.php
+++ application/plugins/generic/data/freehand/Class.php
@@ -14,34 +14,6 @@ class FreehandPluginController extends Aitsu_Adm_Plugin_Controller {
 		
 		$this->_helper->layout->disableLayout();
 		header("Content-type: text/javascript");
-
-		$this->user = Aitsu_Adm_User :: getInstance();
-
-		$results = Aitsu_Db :: fetchCol('' .
-		'select table_name from information_schema.tables ' .
-		'where table_schema = :schema ' .
-		'order by table_name', array (
-			':schema' => Aitsu_Registry :: get()->config->database->params->dbname
-		));
-
-		if ($results) {
-			foreach ($results as $table) {
-				$table = preg_replace('/^[a-zA-Z]*_/', '_', $table);
-				if ($this->user->isAllowed(array (
-						'area' => 'data.table.' . $table,
-						'action' => 'view'
-					)) || $this->user->isAllowed(array (
-						'area' => 'data.table.' . $table,
-						'action' => 'crud'
-					))) {
-					$tables[] = Aitsu_Db :: getInstance()->prefix($table);
-				}
-			}
-		}
-
-		$this->view->placeholder('left')->set($this->view->partial('subnav.phtml', array (
-			'tables' => $tables
-		)));
 	}
 
 	public function indexAction() {
@@ -68,4 +40,33 @@ class FreehandPluginController extends Aitsu_Adm_Plugin_Controller {
 		}
 	}
 
+	protected function _getTables() {
+		
+		$user = Aitsu_Adm_User :: getInstance();
+
+		$results = Aitsu_Db :: fetchCol('' .
+		'select table_name from information_schema.tables ' .
+		'where table_schema = :schema ' .
+		'order by table_name', array (
+			':schema' => Aitsu_Registry :: get()->config->database->params->dbname
+		));
+
+		$tables = array();
+		if ($results) {
+			foreach ($results as $table) {
+				$table = preg_replace('/^[a-zA-Z]*_/', '_', $table);
+				if ($user->isAllowed(array (
+						'area' => 'data.table.' . $table,
+						'action' => 'view'
+					)) || $user->isAllowed(array (
+						'area' => 'data.table.' . $table,
+						'action' => 'crud'
+					))) {
+					$tables[] = Aitsu_Db :: getInstance()->prefix($table);
+				}
+			}
+		}
+		
+		return $tables;
+	} 
 }
