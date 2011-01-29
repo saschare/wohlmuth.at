432f41fafce353dcb0f0dadf24a148bb90df0d92
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-01-29 14:48:59 +0100

    Added new modules.

---
+++ application/modules/List/Download/Class.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/**
+ * @author Andreas Kummer, w3concepts AG
+ * @copyright Copyright © 2010, w3concepts AG
+ */
+
+class Module_List_Download_Class extends Aitsu_Ee_Module_Abstract {
+	
+	public static function init($context) {
+
+		$instance = new self();
+		$index = $context['index'];
+
+		$output = '';
+		if ($instance->_get('List_Download' . $index, $output)) {
+			return $output;
+		}
+
+		$view = $instance->_getView();
+
+		$files = Aitsu_Content_Config_Media :: set($index, 'DownloadList', 'Files');
+		$view->files = Aitsu_Persistence_View_Media :: byFileName(Aitsu_Registry :: get()->env->idart, $files);
+
+		$output = $view->render('index.phtml');
+		$instance->_save($output, 'eternal');
+
+		return $output;
+	}
+}
