863c0abd3333012210e9b2dd7c7b510fa86c5034
Christian Kehres <c.kehres@webtischlerei.de>
2011-04-07 16:56:04 +0200

    changed some header informations and removed expire header and max-age header

--- index.php
+++ index.php
@@ -1,39 +1,34 @@
 <?php
 
-
 /**
  * @author Anreas Kummer, w3concepts AG
+ * @author Christian Kehres, webtischlerei
  * @copyright Copyright &copy; 2010, w3concepts AG
+ * @copyright Copyright &copy; 2011, webtischlerei
  */
-
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
 
 define('REQUEST_START', microtime(true));
-$request = array_merge($_GET, $_POST, array (
-	$_SERVER['HTTP_HOST']
-));
+$request = array_merge($_GET, $_POST, array(
+            $_SERVER['HTTP_HOST']
+        ));
 ksort($request);
-define('REQUEST_HASH', md5(serialize($request)));
-unset ($request);
+define('REQUEST_HASH', hash('md4', serialize($request)));
+unset($request);
 define('CACHE_PATH', APPLICATION_PATH . '/data/pagecache');
 
 require_once (realpath('./library/Aitsu/Bootstrap.php'));
 $content = Aitsu_Bootstrap :: run()->getOutput();
-
-$maxage = 10;
-
 $etag = hash('md4', $content);
-if (isset ($_SERVER['HTTP_IF_NONE_MATCH']) && $etag === $_SERVER['HTTP_IF_NONE_MATCH']) {
-	header("Pragma: public");
-	header("Cache-Control: max-age=" . $maxage);
-	header("ETag: {$etag}");
-	header("HTTP/1.1 304 Not Modified");
-	header("Connection: Close");
-	exit (0);
-}
 
 header("Pragma: public");
-header("Cache-Control: max-age=" . $maxage);
 header("ETag: {$etag}");
+
+if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $etag === $_SERVER['HTTP_IF_NONE_MATCH']) {
+    header("HTTP/1.1 304 Not Modified");
+    header("Connection: Close");
+    exit(0);
+}
+
 echo $content;
-exit (0);
+exit(0);
