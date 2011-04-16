08c4d815f546401b06e4335db3f9e430737e0018
Christian Kehres <c.kehres@webtischlerei.de>
2011-04-16 19:10:16 +0800

    Aitsu_Logging_Activity "basic" structure

---
+++ library/Aitsu/Logging/Activity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Christian Kehres, webtischlerei
+ * @copyright Copyright &copy; 2011, webtischlerei
+ */
+
+class Aitsu_Logging_Activity {
+
+	public static function log() {
+
+        }
+}
