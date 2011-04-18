4f19847525a05ab790288aed14999ff1131c4c1d
Christian Kehres <c.kehres@webtischlerei.de>
2011-04-18 14:52:35 +0800

    added ToDoList on Dashboard

---
+++ application/plugins/dashboard/toDoList/Class.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @author Christian Kehres, webtischlerei
+ * @copyright Copyright &copy; 2011, webtischlerei
+ */
+class toDoListDashboardController extends Aitsu_Adm_Plugin_Controller {
+    const ID = '4da983e3-2cd0-449a-90ad-24117f000101';
+
+    public function init() {
+
+        $this->_helper->layout->disableLayout();
+        header("Content-type: text/javascript");
+    }
+
+    public static function register() {
+
+        return (object) array(
+            'name' => 'toDoList',
+            'tabname' => Aitsu_Translate :: _('ToDo List'),
+            'enabled' => true,
+            'id' => self :: ID
+        );
+    }
+
+    public function indexAction() {
+
+    }
+
+    public function storeAction() {
+
+        $data = Aitsu_Db::fetchAll("
+            SELECT
+                `todo`.`title`,
+                `todo`.`description`,
+                DATE_FORMAT(`todo`.`duedate`, '%d.%m.%Y') AS `duedate`,
+                `art`.`pagetitle`
+            FROM
+                `_todo` AS `todo`
+            INNER JOIN
+                `_art_lang` AS `art` ON `art`.`idartlang` = `todo`.`idartlang`
+            WHERE (
+                `todo`.`duedate` < 'NOW()'
+                OR
+                `todo`.`status` =0
+            )
+            ORDER BY
+                `todo`.`duedate` ASC
+            ");
+
+        $this->_helper->json((object) array(
+                    'data' => $data
+        ));
+    }
+
+}
