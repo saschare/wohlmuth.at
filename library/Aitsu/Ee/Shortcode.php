652a8db70d9fdcd18f4d701446d6ded3d1cd7572
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-06-14 11:12:10 +0200

    Reformated changes made by C. Kehres.

--- library/Aitsu/Ee/Shortcode.php
+++ library/Aitsu/Ee/Shortcode.php
@@ -1,122 +1,124 @@
 <?php
 
+
 /**
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2010, w3concepts AG
  */
+
 class Aitsu_Ee_Shortcode {
 
-    protected function __construct() {
-        
-    }
+	protected function __construct() {
 
-    public static function getInstance() {
+	}
 
-        static $instance;
+	public static function getInstance() {
 
-        if (!isset($instance)) {
-            $instance = new self();
-        }
+		static $instance;
 
-        return $instance;
-    }
+		if (!isset ($instance)) {
+			$instance = new self();
+		}
 
-    /**
-     * @deprecated 0.9.2 - 07.10.2010
-     */
-    public function getContent($params) {
+		return $instance;
+	}
 
-        return '';
-    }
+	/**
+	 * @deprecated 0.9.2 - 07.10.2010
+	 */
+	public function getContent($params) {
 
-    public function evalModule($method, $params, $client, $index, $current = true) {
+		return '';
+	}
 
-        $profileDetails = new stdClass();
-        Aitsu_Profiler :: profile($method . ':' . $index);
+	public function evalModule($method, $params, $client, $index, $current = true) {
 
-        $returnValue = '';
-        Aitsu_Content_Edit :: isBlock(true);
+		$profileDetails = new stdClass();
+		Aitsu_Profiler :: profile($method . ':' . $index);
 
-        $files = array(
-            'Skin_Module' => APPLICATION_PATH . "/skins/" . Aitsu_Registry :: get()->config->skin . "/module/" . str_replace('.', '/', $method) . "/Class.php",
-            'Local_Module' => realpath(APPLICATION_PATH . '/../library/Local/Module/' . str_replace('.', '/', $method) . '/Class.php'),
-            'Comm_Module' => realpath(APPLICATION_PATH . '/../library/Comm/Module/' . str_replace('.', '/', $method) . '/Class.php'),
-            'Module' => APPLICATION_PATH . '/modules/' . str_replace('.', '/', $method) . '/Class.php',
-            'Aitsu_Ee_Module' => realpath(APPLICATION_PATH . '/../library/Aitsu/Ee/Module/' . str_replace('.', '/', $method) . '/Class.php')
-        );
+		$returnValue = '';
+		Aitsu_Content_Edit :: isBlock(true);
 
-        $exists = false;
+		$files = array (
+			'Skin_Module' => APPLICATION_PATH . "/skins/" . Aitsu_Registry :: get()->config->skin . "/module/" . str_replace('.', '/', $method) . "/Class.php",
+			'Local_Module' => realpath(APPLICATION_PATH . '/../library/Local/Module/' . str_replace('.', '/', $method) . '/Class.php'),
+			'Comm_Module' => realpath(APPLICATION_PATH . '/../library/Comm/Module/' . str_replace('.', '/', $method) . '/Class.php'),
+			'Module' => APPLICATION_PATH . '/modules/' . str_replace('.', '/', $method) . '/Class.php',
+			'Aitsu_Ee_Module' => realpath(APPLICATION_PATH . '/../library/Aitsu/Ee/Module/' . str_replace('.', '/', $method) . '/Class.php')
+		);
 
-        foreach ($files as $prefix => $file) {
-            if (file_exists($file)) {
-                $exists = true;
-                $profileDetails->source = $prefix . '_' . str_replace('.', '_', $method) . '_Class';
-                include_once $file;
-                $returnValue = call_user_func(array(
-                    $profileDetails->source,
-                    'init'
-                        ), array(
-                    'index' => $index,
-                    'params' => $params
-                        ));
-                break;
-            }
-        }
+		$exists = false;
 
-        if (!$exists) {
-            Aitsu_Profiler :: profile($method . ':' . $index, (object) array(
-                        'source' => 'not found'
-            ));
-            if (Aitsu_Registry :: isEdit()) {
-                return '<strong>' . sprintf(Aitsu_Registry :: translator()->translate('// The ShortCode \'%s\' does not exist. //'), $method) . '</strong>';
-            } else {
-                return '';
-            }
-        }
+		foreach ($files as $prefix => $file) {
+			if (file_exists($file)) {
+				$exists = true;
+				$profileDetails->source = $prefix . '_' . str_replace('.', '_', $method) . '_Class';
+				include_once $file;
+				$returnValue = call_user_func(array (
+					$profileDetails->source,
+					'init'
+				), array (
+					'index' => $index,
+					'params' => $params
+				));
+				break;
+			}
+		}
 
-        if (is_object($returnValue)) {
-            $index = $returnValue->index;
-            $returnValue = $returnValue->out;
-        }
+		if (!$exists) {
+			Aitsu_Profiler :: profile($method . ':' . $index, (object) array (
+				'source' => 'not found'
+			));
+			if (Aitsu_Registry :: isEdit()) {
+				return '<strong>' . sprintf(Aitsu_Registry :: translator()->translate('// The ShortCode \'%s\' does not exist. //'), $method) . '</strong>';
+			} else {
+				return '';
+			}
+		}
 
-        Aitsu_Profiler :: profile($method . ':' . $index, $profileDetails);
+		if (is_object($returnValue)) {
+			$index = $returnValue->index;
+			$returnValue = $returnValue->out;
+		}
 
-        if (Aitsu_Registry :: isBoxModel() && !Aitsu_Content_Edit :: noEdit($method)) {
-            $returnValue = '<shortcode method="' . $method . '" index="' . $index . '">' . $returnValue . '</shortcode>';
-        } else
-        if (Aitsu_Application_Status :: isStructured()) {
-            $startmarker = '<!--fragment:start ' . $method . '-' . $index . '-->';
-            $endmarker = '<!--fragment:end ' . $method . '-' . $index . '-->';
-            $returnValue = $startmarker . $returnValue . $endmarker;
-        } else
-        if (Aitsu_Registry :: isEdit() && !Aitsu_Content_Edit :: noEdit($method)) {
-            $isBlock = Aitsu_Content_Edit :: isBlock();
-            if ($isBlock === true) {
-                $returnValue = '<div id="' . $method . '-' . $index . '-' . Aitsu_Registry :: get()->env->idartlang . '" class="aitsu_editable"><div class="aitsu_hover">' . $returnValue . '</div></div>';
-            } else {
-                $returnValue = '<span id="' . $method . '-' . $index . '-' . Aitsu_Registry :: get()->env->idartlang . '" class="aitsu_editable" style="display:inline;"><span class="aitsu_hover">' . $returnValue . '</span></span>';
-            }
-        }
+		Aitsu_Profiler :: profile($method . ':' . $index, $profileDetails);
 
-        return $returnValue;
-    }
+		if (Aitsu_Registry :: isBoxModel() && !Aitsu_Content_Edit :: noEdit($method)) {
+			$returnValue = '<shortcode method="' . $method . '" index="' . $index . '">' . $returnValue . '</shortcode>';
+		} else
+			if (Aitsu_Application_Status :: isStructured()) {
+				$startmarker = '<!--fragment:start ' . $method . '-' . $index . '-->';
+				$endmarker = '<!--fragment:end ' . $method . '-' . $index . '-->';
+				$returnValue = $startmarker . $returnValue . $endmarker;
+			} else
+				if (Aitsu_Registry :: isEdit() && !Aitsu_Content_Edit :: noEdit($method)) {
+					$isBlock = Aitsu_Content_Edit :: isBlock();
+					if ($isBlock === true) {
+						$returnValue = '<div id="' . $method . '-' . $index . '-' . Aitsu_Registry :: get()->env->idartlang . '" class="aitsu_editable"><div class="aitsu_hover">' . $returnValue . '</div></div>';
+					} else {
+						$returnValue = '<span id="' . $method . '-' . $index . '-' . Aitsu_Registry :: get()->env->idartlang . '" class="aitsu_editable" style="display:inline;"><span class="aitsu_hover">' . $returnValue . '</span></span>';
+					}
+				}
 
-    /**
-     * The magic __get method is some kind of misused in this case. Instead
-     * of allowing access to non-public members of the object it returns the
-     * names of the parameters of the specified method. False is returned, if
-     * the method does not exist and null, if the specification is not made
-     * or the method does not expect any parameters. In any other case the
-     * method returns an array containing the names of the parameters that are
-     * expected by the method.
-     * @param String Name of the method.
-     * @return Mixed False, null or an array.
-     *
-     * @deprecated 0.9.3 - 13.10.2010
-     */
-    public function __get($key) {
+		return $returnValue;
+	}
 
-        return false;
-    }
+	/**
+	 * The magic __get method is some kind of misused in this case. Instead
+	 * of allowing access to non-public members of the object it returns the
+	 * names of the parameters of the specified method. False is returned, if
+	 * the method does not exist and null, if the specification is not made
+	 * or the method does not expect any parameters. In any other case the
+	 * method returns an array containing the names of the parameters that are
+	 * expected by the method.
+	 * @param String Name of the method.
+	 * @return Mixed False, null or an array.
+	 *
+	 * @deprecated 0.9.3 - 13.10.2010
+	 */
+	public function __get($key) {
+
+		return false;
+	}
 
 }
--- library/Aitsu/Translate.php
+++ library/Aitsu/Translate.php
@@ -1,170 +1,171 @@
 <?php
 
+
 /**
- * aitsu translation class.
- * 
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2010, w3concepts AG
- * 
- * {@id $Id: Translate.php 18800 2010-09-16 21:03:42Z akm $}
  */
+
 class Aitsu_Translate {
 
-    protected $translationMap = array();
-    protected $idlang = null;
+	protected $translationMap = array ();
+	protected $idlang = null;
 
-    protected function __construct() {
+	protected function __construct() {
 
-        $this->idlang = Aitsu_Registry :: get()->env->idlang;
+		$this->idlang = Aitsu_Registry :: get()->env->idlang;
 
-        $this->_readTranslationData();
-    }
+		$this->_readTranslationData();
+	}
 
-    protected static function _getInstance() {
+	protected static function _getInstance() {
 
-        static $instance;
+		static $instance;
 
-        if (!isset($instance)) {
-            $instance = new self();
-        }
+		if (!isset ($instance)) {
+			$instance = new self();
+		}
 
-        return $instance;
-    }
+		return $instance;
+	}
 
-    public static function _($text) {
+	public static function _($text) {
 
-        $instance = self :: _getInstance();
+		$instance = self :: _getInstance();
 
-        if (!isset($instance->translationMap[$text]) || strlen($instance->translationMap[$text]) == 0) {
-            return $text;
-        }
+		if (!isset ($instance->translationMap[$text]) || strlen($instance->translationMap[$text]) == 0) {
+			return $text;
+		}
 
-        return $instance->translationMap[$text];
-    }
+		return $instance->translationMap[$text];
+	}
 
-    protected function _readTranslationData() {
+	protected function _readTranslationData() {
 
-        $results = Aitsu_Db :: fetchAll('' .
-                        'select tkey, tvalue from _translate ' .
-                        'where idlang = ? ', array(
-                    $this->idlang
-                ));
+		$results = Aitsu_Db :: fetchAll('' .
+		'select tkey, tvalue from _translate ' .
+		'where idlang = ? ', array (
+			$this->idlang
+		));
 
-        if (!$results) {
-            return;
-        }
+		if (!$results) {
+			return;
+		}
 
-        foreach ($results as $result) {
-            $this->translationMap[$result['tkey']] = $result['tvalue'];
-        }
-    }
+		foreach ($results as $result) {
+			$this->translationMap[$result['tkey']] = $result['tvalue'];
+		}
+	}
 
-    public static function populate($idlang) {
+	public static function populate($idlang) {
 
-        Aitsu_Db :: query('update _translate set obsolete = now() where obsolete is null and idlang = ? ', array(
-            $idlang
-        ));
+		Aitsu_Db :: query('update _translate set obsolete = now() where obsolete is null and idlang = ? ', array (
+			$idlang
+		));
 
-        self :: _populateFromClasses($idlang);
-    }
+		self :: _populateFromClasses($idlang);
+	}
 
-    protected static function _populateFromClasses($idlang) {
+	protected static function _populateFromClasses($idlang) {
 
-        $files = scandir(APPLICATION_LIBPATH);
+		$files = scandir(APPLICATION_LIBPATH);
 
-        $dirs = array(
-            APPLICATION_PATH . '/plugins',
-            APPLICATION_PATH . '/skins',
-            APPLICATION_PATH . '/modules'
-        );
+		$dirs = array (
+			APPLICATION_PATH . '/plugins',
+			APPLICATION_PATH . '/skins',
+			APPLICATION_PATH . '/modules'
+		);
 
-        foreach ($dirs as $dir) {
-            if (is_dir($dir)) {
-                $files = array_merge($files, self::_scanDir($dir, array('php', 'phtml')));
-            }
-        }
+		foreach ($dirs as $dir) {
+			if (is_dir($dir)) {
+				$files = array_merge($files, self :: _scanDir($dir, array (
+					'php',
+					'phtml'
+				)));
+			}
+		}
 
-        foreach ($files as $file) {
-            $content = file_get_contents($file);
-            if (preg_match_all("@Aitsu_Translate\\s*\\:{2}\\s*_\\(\\s*(['\\\"])(.*?)(?:\\1\\s*\\))@", $content, $matches) > 0) {
-                foreach ($matches[2] as $key) {
-                    $key = trim($key);
-                    if (Aitsu_Db :: fetchOne('' .
-                                    'select count(*) from _translate where tkey = ? and idlang = ? ', array(
-                                $key,
-                                $idlang
-                            )) == 0) {
-                        Aitsu_Db :: query('' .
-                                'insert into _translate ' .
-                                '(idlang, tkey) ' .
-                                'values ' .
-                                '(?, ?) ', array(
-                            $idlang,
-                            $key
-                        ));
-                    } else {
-                        Aitsu_Db :: query('update _translate set obsolete = null where tkey = ? and idlang = ? ', array(
-                            $key,
-                            $idlang
-                        ));
-                    }
-                }
-            }
-        }
-    }
+		foreach ($files as $file) {
+			$content = file_get_contents($file);
+			if (preg_match_all("@Aitsu_Translate\\s*\\:{2}\\s*_\\(\\s*(['\\\"])(.*?)(?:\\1\\s*\\))@", $content, $matches) > 0) {
+				foreach ($matches[2] as $key) {
+					$key = trim($key);
+					if (Aitsu_Db :: fetchOne('' .
+						'select count(*) from _translate where tkey = ? and idlang = ? ', array (
+							$key,
+							$idlang
+						)) == 0) {
+						Aitsu_Db :: query('' .
+						'insert into _translate ' .
+						'(idlang, tkey) ' .
+						'values ' .
+						'(?, ?) ', array (
+							$idlang,
+							$key
+						));
+					} else {
+						Aitsu_Db :: query('update _translate set obsolete = null where tkey = ? and idlang = ? ', array (
+							$key,
+							$idlang
+						));
+					}
+				}
+			}
+		}
+	}
 
-    protected static function _scanDir($dir, $extensions) {
+	protected static function _scanDir($dir, $extensions) {
 
-        $return = array();
-        $files = scandir($dir);
+		$return = array ();
+		$files = scandir($dir);
 
-        foreach ($files as $file) {
-            $pathInfo = pathinfo($file);
-            $extension = isset($pathInfo['extension']) ? $pathInfo['extension'] : '';
-            if ($file != '.' && $file != '..' && in_array($extension, $extensions)) {
-                $return[] = $dir . '/' . $file;
-            }
-            if ($file != '.' && $file != '..' && is_dir($dir . '/' . $file)) {
-                $return = array_merge($return, self :: _scanDir($dir . '/' . $file, $extensions));
-            }
-        }
+		foreach ($files as $file) {
+			$pathInfo = pathinfo($file);
+			$extension = isset ($pathInfo['extension']) ? $pathInfo['extension'] : '';
+			if ($file != '.' && $file != '..' && in_array($extension, $extensions)) {
+				$return[] = $dir . '/' . $file;
+			}
+			if ($file != '.' && $file != '..' && is_dir($dir . '/' . $file)) {
+				$return = array_merge($return, self :: _scanDir($dir . '/' . $file, $extensions));
+			}
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    public static function getTranslationData($idlang, $pattern = '*') {
+	public static function getTranslationData($idlang, $pattern = '*') {
 
-        $pattern = str_replace('*', '%', $pattern);
+		$pattern = str_replace('*', '%', $pattern);
 
-        $return = Aitsu_Db :: fetchAll('' .
-                        'select * from _translate ' .
-                        'where ' .
-                        '	idlang = ? ' .
-                        '	and ( ' .
-                        '		tkey like ? ' .
-                        '		or tvalue like ? ' .
-                        '		) ' .
-                        '	and obsolete is null ' .
-                        'order by tkey asc ', array(
-                    $idlang,
-                    $pattern,
-                    $pattern
-                ));
+		$return = Aitsu_Db :: fetchAll('' .
+		'select * from _translate ' .
+		'where ' .
+		'	idlang = ? ' .
+		'	and ( ' .
+		'		tkey like ? ' .
+		'		or tvalue like ? ' .
+		'		) ' .
+		'	and obsolete is null ' .
+		'order by tkey asc ', array (
+			$idlang,
+			$pattern,
+			$pattern
+		));
 
-        if (!$return) {
-            return array();
-        }
+		if (!$return) {
+			return array ();
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    public static function translate($text) {
+	public static function translate($text) {
 
-        if (!is_a(Aitsu_Registry :: get()->Zend_Translate, 'Zend_Translate')) {
-            return $text;
-        }
+		if (!is_a(Aitsu_Registry :: get()->Zend_Translate, 'Zend_Translate')) {
+			return $text;
+		}
 
-        return Aitsu_Registry :: get()->Zend_Translate->translate($text);
-    }
+		return Aitsu_Registry :: get()->Zend_Translate->translate($text);
+	}
 
 }
