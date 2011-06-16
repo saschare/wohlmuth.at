79497b0c1e8d039fa0ce5e1f7b2c65bab83f9fe8
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-06-16 13:16:38 +0200

    Fine tuning of to-do dashboard plugin.

--- application/plugins/dashboard/toDoList/Class.php
+++ application/plugins/dashboard/toDoList/Class.php
@@ -1,62 +1,62 @@
 <?php
 
+
 /**
  * @author Christian Kehres, webtischlerei
+ * @author Andreas Kummer, w3concepts AG
+ * 
  * @copyright Copyright &copy; 2011, webtischlerei
+ * @copyright Copyright &copy; 2011, w3concepts AG
  */
+
 class toDoListDashboardController extends Aitsu_Adm_Plugin_Controller {
-    const ID = '4da983e3-2cd0-449a-90ad-24117f000101';
 
-    public function init() {
+	const ID = '4df9e2fc-fac4-4b01-9db5-09167f000101';
 
-        $this->_helper->layout->disableLayout();
-        header("Content-type: text/javascript");
-    }
+	public function init() {
 
-    public static function register() {
+		$this->_helper->layout->disableLayout();
+		header("Content-type: text/javascript");
+	}
 
-        return (object) array(
-            'name' => 'toDoList',
-            'tabname' => Aitsu_Translate :: _('To-do-List'),
-            'enabled' => true,
-            'id' => self :: ID
-        );
-    }
+	public static function register() {
 
-    public function indexAction() {
+		return (object) array (
+			'name' => 'toDoList',
+			'tabname' => Aitsu_Translate :: _('To-do-List'),
+			'enabled' => true,
+			'id' => self :: ID
+		);
+	}
 
-    }
+	public function indexAction() {
 
-    public function storeAction() {
+	}
 
-        $data = Aitsu_Db::fetchAll("
-            SELECT
-                `todo`.`title`,
-                `todo`.`description`,
-                `todo`.`duedate`,
-                `art`.`pagetitle`,
-                `art`.`idart`,
-                NOW() AS `today`
-            FROM
-                `_todo` AS `todo`
-            INNER JOIN
-                `_art_lang` AS `art` ON `art`.`idartlang` = `todo`.`idartlang`
-            WHERE (
-                `todo`.`duedate` < 'NOW()'
-                OR
-                `todo`.`status` =0
-            )
-            AND
-                `todo`.`userid` =:userid
-            ORDER BY
-                `todo`.`duedate` ASC
-            ", array(
-                ':userid' => Aitsu_Adm_User::getInstance()->userid
-            ));
+	public function storeAction() {
 
-        $this->_helper->json((object) array(
-                    'data' => $data
-        ));
-    }
+		$data = Aitsu_Db :: fetchAll('' .
+		'SELECT' .
+		'	todo.title, ' .
+		'	todo.description, ' .
+		'	todo.duedate, ' .
+		'	art.pagetitle, ' .
+		'	art.title as articletitle, ' .
+		'	art.idart, ' .
+		'	now() as today ' .
+		'from _todo todo ' .
+		'left join _art_lang art on art.idartlang = todo.idartlang ' .
+		'where ' .
+		'	(todo.duedate < now() or todo.status = 0) ' .
+		'	and todo.userid = :userid ' .
+		'order by ' .
+		'	todo.duedate asc', array (
+			':userid' => Aitsu_Adm_User :: getInstance()->userid
+		));
+
+		$this->_helper->json((object) array (
+			'data' => $data
+		));
+	}
 
 }
