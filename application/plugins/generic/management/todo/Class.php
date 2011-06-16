1b71c8e565e914b3a5fc3454a3d9686abc5e3291
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-06-16 14:29:53 +0200

    Added generic todo list plugin.

--- application/plugins/generic/management/todo/Class.php
+++ application/plugins/generic/management/todo/Class.php
@@ -2,35 +2,20 @@
 
 
 /**
- * @author Christian Kehres, webtischlerei
  * @author Andreas Kummer, w3concepts AG
- * 
- * @copyright Copyright &copy; 2011, webtischlerei
  * @copyright Copyright &copy; 2011, w3concepts AG
  */
 
-class toDoListDashboardController extends Aitsu_Adm_Plugin_Controller {
-
-	const ID = '4df9e2fc-fac4-4b01-9db5-09167f000101';
+class TodoPluginController extends Aitsu_Adm_Plugin_Controller {
 
 	public function init() {
 
 		$this->_helper->layout->disableLayout();
-		header("Content-type: text/javascript");
-	}
-
-	public static function register() {
-
-		return (object) array (
-			'name' => 'toDoList',
-			'tabname' => Aitsu_Translate :: _('To-do-List'),
-			'enabled' => true,
-			'id' => self :: ID
-		);
 	}
 
 	public function indexAction() {
 
+		header("Content-type: text/javascript");
 	}
 
 	public function storeAction() {
@@ -43,15 +28,19 @@ class toDoListDashboardController extends Aitsu_Adm_Plugin_Controller {
 		'	art.pagetitle, ' .
 		'	art.title as articletitle, ' .
 		'	art.idart, ' .
-		'	now() as today ' .
+		'	now() as today, ' .
+		'	catlang.url ' .
 		'from _todo todo ' .
 		'left join _art_lang art on art.idartlang = todo.idartlang ' .
+		'left join _cat_art catart on art.idart = catart.idart ' .
+		'left join _cat_lang catlang on catart.idcat = catlang.idcat and catlang.idlang = :idlang ' .
 		'where ' .
 		'	todo.status = 0 ' .
 		'	and todo.userid = :userid ' .
 		'order by ' .
 		'	todo.duedate asc', array (
-			':userid' => Aitsu_Adm_User :: getInstance()->userid
+			':userid' => Aitsu_Adm_User :: getInstance()->userid,
+			':idlang' => Aitsu_Registry :: get()->session->currentLanguage
 		));
 
 		$this->_helper->json((object) array (
