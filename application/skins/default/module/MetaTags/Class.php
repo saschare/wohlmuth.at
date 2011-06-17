0e43763b8312c21fd9e471985136f19547778078
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-06-17 19:47:39 +0200

    Added media, fixed warnings and updated db dump.

--- application/skins/default/module/MetaTags/Class.php
+++ application/skins/default/module/MetaTags/Class.php
@@ -2,31 +2,13 @@
 
 /**
  * @author Andreas Kummer, w3concepts AG
+ * @author Frank Ammari, Ammari & Ammari GbR
  * @copyright Copyright &copy; 2010, w3concepts AG
- * @Editor Frank Ammari, Ammari & Ammari GbR
- * 
- * {@id $Id: Class.php 18266 2010-08-23 10:07:27Z akm $}
+ * @copyright Copyright &copy; 2011, Ammari & Ammari GbR 
  */
 
 class Skin_Module_MetaTags_Class extends Aitsu_Ee_Module_Abstract {
 	
-	public static function about() {
-
-		return (object) array (
-			'name' => 'Meta tags',
-			'description' => Zend_Registry :: get('Zend_Translate')->translate('Returns the meta tags of the current article.'),
-			'type' => 'Header',
-			'author' => (object) array (
-				'name' => 'Andreas Kummer',
-				'copyright' => 'w3concepts AG'
-			),
-			'version' => '1.0.0',
-			'status' => 'stable',
-			'url' => null,
-			'id' => 'a072536f-c565-11df-851a-0800200c9a66'
-		);
-	}
-
 	public static function init($context) {
 
 		$instance = new self();
