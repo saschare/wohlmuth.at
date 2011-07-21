2cdd3012d8fe672b0ca2a9e497816d2114681198
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-07-21 15:12:24 +0200

    Added templates to Image module.

--- application/modules/Image/Class.php
+++ application/modules/Image/Class.php
@@ -14,7 +14,9 @@ class Module_Image_Class extends Aitsu_Module_Abstract {
 
 		$template = empty ($this->_params->template) ? 'index' : $this->_params->template;
 
-		$images = Aitsu_Content_Config_Media :: set($this->_index, 'media', 'Media');
+		$images = Aitsu_Content_Config_Media :: set($this->_index, 'Image.Media', 'Media');
+		
+		$template = Aitsu_Content_Config_Radio :: set($this->_index, 'Image.Template', '', $this->_getTemplates(), 'Template');
 
 		$view = $this->_getView();
 		$view->images = Aitsu_Persistence_View_Media :: byFileName(Aitsu_Registry :: get()->env->idart, $images);
