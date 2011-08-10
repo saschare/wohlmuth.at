8ed2a60f646f7b6361be5cee72b4218d63902c5b
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-08-10 21:59:28 +0200

    Added array util class.

---
+++ library/Aitsu/Util/Array.php
@@ -0,0 +1,51 @@
+<?php
+
+
+/**
+ * @author Andreas Kummer, w3concepts AG
+ * @copyright Copyright &copy; 2011, w3concepts AG
+ */
+class Aitsu_Util_Array {
+
+	public static function shiftAndMix($array, $columns, $length = null) {
+		
+		$returnValue = array ();
+		$tmp = array ();
+		
+		$numberOfItems = count($array);
+		if ($length != null && $length > $numberOfItems) {
+			$numberOfItems = $length;
+		}
+
+		$rows = ceil(count($array) / $columns);
+		$unused = $rows * $columns -count($array);
+
+		$tmp = array ();
+		$col = 0;
+		$counter = 0;
+		foreach ($array as $key => $value) {
+			if ($length != null && $counter >= $length - 1) {
+				break;
+			}
+			$maxEntries = ($columns - $unused > $col) ? $rows : $rows -1;
+			if (count($tmp[$col]) == $maxEntries) {
+				$col++;
+			}
+			$tmp[$col][] = array (
+				$key,
+				$value
+			);
+			$counter++;
+		}
+
+		for ($row = 0; $row < $rows; $row++) {
+			for ($col = 0; $col < $columns; $col++) {
+				if (isset ($tmp[$col][$row])) {
+					$returnValue[$tmp[$col][$row][0]] = $tmp[$col][$row][1];
+				}
+			}
+		}
+
+		return $returnValue;
+	}
+}
