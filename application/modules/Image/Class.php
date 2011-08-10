7d5a0ad8e30ad3183ebba7f0a2d767882ba5fcb8
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-08-10 13:37:04 +0200

    Fixed bug in Image shortcode.

--- application/modules/Image/Class.php
+++ application/modules/Image/Class.php
@@ -2,32 +2,33 @@
 
 
 /**
- * @author Christian Kehres, webtischlerei
- * @copyright Copyright &copy; 2011, webtischlerei
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2011, w3concepts AG
  */
 
 class Module_Image_Class extends Aitsu_Module_Abstract {
 
-	protected function _init() {
+	protected function _main() {
 
 		$template = empty ($this->_params->template) ? 'index' : $this->_params->template;
 
 		$images = Aitsu_Content_Config_Media :: set($this->_index, 'Image.Media', 'Media');
-		
+
 		$template = Aitsu_Content_Config_Radio :: set($this->_index, 'Image.Template', '', $this->_getTemplates(), 'Template');
-		
-		if (empty($template)) {
+
+		if (empty ($template) || empty ($images)) {
 			return '';
 		}
 
 		$view = $this->_getView();
 		$view->images = Aitsu_Persistence_View_Media :: byFileName(Aitsu_Registry :: get()->env->idart, $images);
 
-		$output = $view->render($template . '.phtml');
+		return $view->render($template . '.phtml');
+	}
 
-		return $output;
+	protected function _cachingPeriod() {
+
+		return 60 * 60 * 24 * 365;
 	}
 
 }
