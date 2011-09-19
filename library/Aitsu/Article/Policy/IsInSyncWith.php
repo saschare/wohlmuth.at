aff826b530e7dccb1749f2936d4ab2e55131e229
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-09-19 17:12:16 +0200

    Policy basics implemented.

--- library/Aitsu/Article/Policy/IsInSyncWith.php
+++ library/Aitsu/Article/Policy/IsInSyncWith.php
@@ -4,8 +4,10 @@
 /**
  * @author Andreas Kummer, w3concepts AG
  * @copyright Copyright &copy; 2011, w3concepts AG
+ * 
+ * This class has not yet been tested.
  */
-class Aitsu_Article_Policy_ExistsInLanguage extends Aitsu_Article_Policy_Abstract {
+class Aitsu_Article_Policy_IsInSyncWith extends Aitsu_Article_Policy_Abstract {
 
 	protected function _evalStatement($statement) {
 
@@ -40,10 +42,10 @@ class Aitsu_Article_Policy_ExistsInLanguage extends Aitsu_Article_Policy_Abstrac
 		/*
 		 * Following conditions have to be met to fullfill:
 		 * 
-		 * - the referenced article exists,
-		 * - the referenced article is online if the current article is online,
-		 * - the referenced article is offline if the current article is offline,
-		 * - the referenced article is published
+		 * - the base article is online if the current article is online,
+		 * - the base article is offline if the current article is offline,
+		 * - the publication time of the base article is older than that
+		 *   of the current article. 
 		 */
 
 		if (Aitsu_Db :: fetchOne('' .
@@ -61,10 +63,10 @@ class Aitsu_Article_Policy_ExistsInLanguage extends Aitsu_Article_Policy_Abstrac
 		if (!$this->_statement->target) {
 			/*
 			 * The specified target language does not|no longer|not yet exist. 
-			 * The policy is therefore assumed not to be fullfilled.
+			 * The policy is therefore assumed to be fullfilled.
 			 */
-			$this->_message = 'target language does not exist.';
-			return false;
+			$this->_message = 'the base article does not exist.';
+			return true;
 		}
 
 		$status = Aitsu_Db :: fetchRow('' .
@@ -82,8 +84,8 @@ class Aitsu_Article_Policy_ExistsInLanguage extends Aitsu_Article_Policy_Abstrac
 		));
 
 		if (!$status) {
-			$this->_message = 'target article does not exist.';
-			return false;
+			$this->_message = 'the base article does not exist.';
+			return true;
 		}
 
 		/*
@@ -93,17 +95,34 @@ class Aitsu_Article_Policy_ExistsInLanguage extends Aitsu_Article_Policy_Abstrac
 			$status['idartlang']
 		);
 
-		if ($status['origonline'] == $status['targetonline']) {
-			return true;
+		if ($status['origonline'] != $status['targetonline']) {
+			if ($status['targetonline'] == 1) {
+				$this->_message = 'the article should be online.';
+			} else {
+				$this->_message = 'the article should be offline.';
+			}
+			return false;
 		}
 
-		if ($status['targetonline'] == 1) {
-			$this->_message = 'target article should be offline.';
-		} else {
-			$this->_message = 'target article should be online.';
+		if (Aitsu_Db :: fetchOne('' .
+			'select count(t.idartlang) ' .
+			'from _pub s ' .
+			'left join _art_lang sartlang on sartlang.idartlang = s.idartlang ' .
+			'left join _art_lang tartlang on sartlang.idart = tartlang.idart ' .
+			'left join _pub t on tartlang.idartlang = t.idartlang ' .
+			'where ' .
+			'	tartlang.idlang = :idlang ' .
+			'	s.idartlang = :idartlang ' .
+			'	and s.status = 1 ' .
+			'	and t.status = 1 ' .
+			'	and s.pubtime > t.pubtime', array (
+				':idartlang' => $this->_idartlang,
+				':idlang' => $this->_statement->target
+			)) == 0) {
+			$this->_message = 'the article \'s publication date is younger that that of the base article.';
 		}
 
-		return false;
+		return true;
 	}
 
 }
