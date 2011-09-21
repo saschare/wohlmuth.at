26f374c6a42bf3181eca080c605b57210ce19d05
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-09-21 09:53:54 +0200

    Version 2.3.8, Revision 4

---
+++ application/modules/Test/Date/Util/SecondsUntilEndOf/Class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * @author Andreas Kummer, w3concepts AG
+ * @copyright Copyright &copy; 2011, w3concepts AG
+ */
+class Module_Test_Date_Util_SecondsUntilEndOf_Class extends Aitsu_Module_Abstract {
+
+	protected function _main() {
+
+		$view = $this->_getView();
+
+		return $view->render('index.phtml');
+	}
+
+}
