a387ff9b88ab4fe1c17f7b8895496223aae50f46
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2011-12-20 21:03:09 +0100

    Version 2.3.11, Revision 6

--- library/Aitsu/Application/Status.php
+++ library/Aitsu/Application/Status.php
@@ -168,7 +168,7 @@ class Aitsu_Application_Status {
 
 	public static function version() {
 
-		$version = '$version/2.3.11/revision/5$';
+		$version = '$version/2.3.11/revision/6$';
 		
 		$version = str_replace(array (
 			'version/',
