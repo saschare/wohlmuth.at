4a850970c1f1bdffb642f9ffe15aa6b8741715cb
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-01-13 21:21:27 +0100

    Reformated only.

--- library/Aitsu/Config/Ini.php
+++ library/Aitsu/Config/Ini.php
@@ -4,9 +4,8 @@
 /**
  * @author Christian Kehres, webtischlerei.de
  * @author Andreas Kummer, w3concepts AG
- * @copyright Copyright &copy; 2010, webtischlerei.de
+ * @copyright Copyright &copy; 2012, w3concepts AG
  */
-
 class Aitsu_Config_Ini {
 
 	protected function __construct() {
@@ -14,11 +13,11 @@ class Aitsu_Config_Ini {
 
 	public static function getInstance($ini) {
 
-                if (!empty($_SERVER['PHP_FCGI_CHILDREN']) || !empty($_SERVER['FCGI_ROLE'])) {
-                    $env = (getenv("REDIRECT_AITSU_ENV") == '' ? 'default' : getenv("REDIRECT_AITSU_ENV"));
-                } else {
-                    $env = (getenv("AITSU_ENV") == '' ? 'default' : getenv("AITSU_ENV"));
-                }
+		if (!empty ($_SERVER['PHP_FCGI_CHILDREN']) || !empty ($_SERVER['FCGI_ROLE'])) {
+			$env = (getenv("REDIRECT_AITSU_ENV") == '' ? 'default' : getenv("REDIRECT_AITSU_ENV"));
+		} else {
+			$env = (getenv("AITSU_ENV") == '' ? 'default' : getenv("AITSU_ENV"));
+		}
 
 		$config = new Zend_Config_Ini('application/configs/config.ini', $env, array (
 			'allowModifications' => true
--- library/Aitsu/Event/Dispatcher.php
+++ library/Aitsu/Event/Dispatcher.php
@@ -3,9 +3,8 @@
 
 /**
  * @author Andreas Kummer, w3concepts AG
- * @copyright Copyright &copy; 2011, w3concepts AG
+ * @copyright Copyright &copy; 2012, w3concepts AG
  */
-
 class Aitsu_Event_Dispatcher {
 
 	protected $_config = null;
@@ -48,11 +47,11 @@ class Aitsu_Event_Dispatcher {
 			return;
 		}
 
-                if (!empty($_SERVER['PHP_FCGI_CHILDREN']) || !empty($_SERVER['FCGI_ROLE'])) {
-                    $env = (getenv("REDIRECT_AITSU_ENV") == '' ? 'default' : getenv("REDIRECT_AITSU_ENV"));
-                } else {
-                    $env = (getenv("AITSU_ENV") == '' ? 'default' : getenv("AITSU_ENV"));
-                }
+		if (!empty ($_SERVER['PHP_FCGI_CHILDREN']) || !empty ($_SERVER['FCGI_ROLE'])) {
+			$env = (getenv("REDIRECT_AITSU_ENV") == '' ? 'default' : getenv("REDIRECT_AITSU_ENV"));
+		} else {
+			$env = (getenv("AITSU_ENV") == '' ? 'default' : getenv("AITSU_ENV"));
+		}
 
 		$this->_config = new Zend_Config_Ini($eventsTable, $env, array (
 			'allowModifications' => true
@@ -136,7 +135,7 @@ class Aitsu_Event_Dispatcher {
 			$config = & $config-> $parts[$i];
 		}
 
-		$config->$parts[$i] = false;
+		$config-> $parts[$i] = false;
 
 		return true;
 	}
