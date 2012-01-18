b7b002629292ded484fc2d2e4400ec6b48449199
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-01-18 13:31:51 +0100

    Moved shortcode HTML to Content.HTML.

--- application/modules/HTML/Class.php
+++ application/modules/HTML/Class.php
@@ -2,24 +2,14 @@
 
 
 /**
+ * The module has been moved to Content.HTML. The present
+ * implementation exists for convenience reasons only. In
+ * new installations use Content.HTML instead.
+ * 
  * @author Andreas Kummer, w3concepts AG
- * @copyright Copyright &copy; 2011, w3concepts AG
+ * @copyright Copyright &copy; 2012, w3concepts AG
  */
+include_once APPLICATION_PATH . '/modules/Content/HTML/Class.php';
 
-class Module_HTML_Class extends Aitsu_Module_Tree_Abstract {
-
-	protected function _init() {
-
-		$return = '';
-		if (Aitsu_Application_Status :: isEdit()) {
-			$return .= '<div style="padding-top:5px; padding-bottom:5px;">';
-		} else {
-			$return .= '<div>';
-		}
-
-		$index = str_replace('_', ' ', $this->_context['rawIndex']);
-		$output = Aitsu_Content_Html :: get($index);
-
-		return $return . $output . '</div>';
-	}
+class Module_HTML_Class extends Module_Content_HTML_Class {	
 }
