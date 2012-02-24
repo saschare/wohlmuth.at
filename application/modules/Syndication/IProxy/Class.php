4dce7ab8493ebae10d4d771f5d3a620260a772f7
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-02-24 18:47:32 +0100

    Finished development. Ready to use.

--- application/modules/Syndication/IProxy/Class.php
+++ application/modules/Syndication/IProxy/Class.php
@@ -12,7 +12,22 @@ class Module_Syndication_IProxy_Class extends Aitsu_Module_Abstract {
 		$view = $this->_getView();
 
 		$page = Aitsu_Content_Config_Link :: set($this->_index, 'IProxy.Source', 'Article', 'Source');
-		
+		$view->module = Aitsu_Content_Config_Text :: set($this->_index, 'IProxy.Module', 'Module', 'Source');
+
+		if (!preg_match('/(\\d+)/', $page, $match) || empty($view->module)) {
+			return '';
+		}
+
+		$view->idartlang = Aitsu_Db :: fetchOne('' .
+		'select idartlang from _art_lang where idart = :idart and idlang = :idlang', array (
+			':idart' => $match[1],
+			':idlang' => Aitsu_Registry :: get()->env->idlang
+		));
+
+		if (!$view->idartlang) {
+			return '';
+		}
+
 		return $view->render('index.phtml');
 	}
 }
