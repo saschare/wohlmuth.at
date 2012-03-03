202c2568c3bcee60f4b7d1f5e16882a8afa851ac
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-03-03 11:52:36 +0100

    Documentation.

--- library/Aitsu/Util/Javascript.php
+++ library/Aitsu/Util/Javascript.php
@@ -3,9 +3,8 @@
 
 /**
  * @author Andreas Kummer, w3concepts AG
- * @copyright Copyright &copy; 2010, w3concepts AG
+ * @copyright Copyright &copy; 2012, w3concepts AG
  */
-
 class Aitsu_Util_Javascript {
 
 	protected $js = array ();
