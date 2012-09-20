65bc1aca7501d564c3bf1362ba728f6628cd919e
Christian Kehres <c.kehres@webtischlerei.de>
2012-09-20 19:35:43 +0200

    Version 1.0.0, Revision 1

---
+++ library/Webtischlerei/Status.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author Andreas Kummer, w3concepts AG
+ * @copyright Copyright &copy; 2011, w3conceps AG
+ */
+class Webtischlerei_Status {
+
+    public static function version() {
+
+        $version = '$version/1.0.0/revision/1$';
+
+        $version = str_replace(array(
+            'version/',
+            '/revision/',
+            '$'
+                ), array(
+            '',
+            '-',
+            ''
+                ), $version);
+
+        return $version;
+    }
+
+}
