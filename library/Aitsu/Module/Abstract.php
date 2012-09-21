e34fe4db9e6922e23352fcd1319662de0048b0d6
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-09-21 17:27:52 +0200

    Change class.php to Class.php on line 285.

--- library/Aitsu/Module/Abstract.php
+++ library/Aitsu/Module/Abstract.php
@@ -282,7 +282,7 @@ abstract class Aitsu_Module_Abstract {
 		);
 		$skinModulePath = APPLICATION_PATH . "/skins/" . (isset (Aitsu_Registry :: get()->config->skin) ? Aitsu_Registry :: get()->config->skin : 'x') . "/module/" . str_replace($search, $replace, $modulePath);
 
-		if (file_exists($skinModulePath . '/class.php')) {
+		if (file_exists($skinModulePath . '/Class.php')) {
 			$view->setScriptPath($skinModulePath);
 		}
 		elseif (file_exists(APPLICATION_PATH . '/modules/' . str_replace('_', '/', substr($class, 7, strlen($class) - 13)))) {
