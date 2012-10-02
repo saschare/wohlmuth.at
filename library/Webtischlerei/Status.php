677e0e49815a22a7230a89b9bf6a957b38067661
Christian Kehres <c.kehres@webtischlerei.de>
2012-10-02 15:17:24 +0200

    Version 1.1.0, Revision 6

--- library/Webtischlerei/Status.php
+++ library/Webtischlerei/Status.php
@@ -8,7 +8,7 @@ class Webtischlerei_Status {
 
     public static function version() {
 
-        $version = '$version/1.1.0/revision/5$';
+        $version = '$version/1.1.0/revision/6$';
 
         return str_replace(array(
             'version/',
