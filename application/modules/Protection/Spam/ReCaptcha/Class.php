c02af267da7ce78671b0361d649501d883ec284d
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-10-12 16:08:40 +0200

    ReCaptcha finished first step.

--- application/modules/Protection/Spam/ReCaptcha/Class.php
+++ application/modules/Protection/Spam/ReCaptcha/Class.php
@@ -8,64 +8,11 @@
 
 class Module_Protection_Spam_ReCaptcha_Class extends Aitsu_Module_Abstract {
 
-	protected $_allowEdit = true;
+	protected $_allowEdit = false;
 
 	protected function _init() {
 
-		$view = $this->_getView();
-
-		$theme = Aitsu_Content_Config_Radio :: set($this->_index, 'ReCaptcha.theme', '', array (
-			'Rot (default)' => 'red',
-			'Weiss' => 'white',
-			'Schwarz' => 'blackglass',
-			'Clean' => 'clean',
-			'Custom' => 'custom'
-		), 'Theme');
-
-		$template = $theme == 'custom' ? 'custom' : 'index';
-		$view->theme = empty ($theme) ? 'red' : $theme;
-
-		if (Aitsu_Application_Status :: isEdit()) {
-			return;
-		}
-
-		$this->_verifyResponse();
-
-		if (Aitsu_User_Status :: isHuman() && !Aitsu_Application_Status :: isPreview()) {
-			/*
-			 * We have to redirect the browser to where the user has
-			 * been before.
-			 */
-			header('Location: ' . Aitsu_User_Status :: getUrl(1));
-			exit;
-		}
-
-		$return = $view->render('init.phtml');
-		$return .= $view->render($template . '.phtml');
-
-		return $return;
+		return $this->_getView()->render('index.phtml');
 	}
 
-	protected function _verifyResponse() {
-
-		if (empty ($_POST) || empty ($_POST['recaptcha_challenge_field']) || empty ($_POST['recaptcha_response_field'])) {
-			return;
-		}
-
-		$client = new Zend_Http_Client('http://www.google.com/recaptcha/api/verify', array (
-			'maxredirects' => 5,
-			'timeout' => 30
-		));
-		$client->setParameterPost(array (
-			'privatekey' => Aitsu_Config :: get('captcha.recaptcha.key.private'),
-			'remoteip' => $_SERVER['REMOTE_ADDR'],
-			'challenge' => $_POST['recaptcha_challenge_field'],
-			'response' => $_POST['recaptcha_response_field']
-		));
-		$response = $client->request('POST');
-
-		if (substr($response->getBody(), 0, 4) == 'true') {
-			Aitsu_User_Status :: isHuman(true);
-		}
-	}
 }
