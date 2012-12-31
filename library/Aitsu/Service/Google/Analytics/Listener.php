c02d816740fa140927d9203453daf2ada2e53fb9
Andreas Kummer, w3concepts AG <a.kummer@wdrei.ch>
2012-12-31 14:58:06 +0100

    Intermedidate dev stage.

---
+++ library/Aitsu/Service/Google/Analytics/Listener.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/**
+ * @author Andreas Kummer, w3concepts AG
+ * @copyright Copyright &copy; 2013, w3concepts AG
+ */
+
+class Aitsu_Service_Google_Analytics_Listener implements Aitsu_Event_Listener_Interface {
+
+	public static function notify(Aitsu_Event_Abstract $event) {
+
+		if (!isset ($event->bootstrap->pageContent)) {
+			return;
+		}
+		
+		if (isset($_GET['anonymous'])) {
+			return;
+		}
+		
+		$js = Aitsu_Service_Google_Analytics :: getScript();
+		if (!empty ($js)) {
+			$event->bootstrap->pageContent = str_replace('</body>', "{$js}</body>", $event->bootstrap->pageContent);
+		}
+	}
+
+}
