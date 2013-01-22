83487294ae33480e8334223478719f2f86eedbb4
Christian Kehres <c.kehres@webtischlerei.de>
2013-01-22 15:28:52 +0100

    minor "Search_Lucene" module changes

--- application/modules/Search/Lucene/Class.php
+++ application/modules/Search/Lucene/Class.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @author Christian Kehres, webtischlerei
  * @copyright Copyright &copy; 2011, webtischlerei
@@ -9,34 +8,32 @@
  */
 class Module_Search_Lucene_Class extends Aitsu_Module_Abstract {
 
-	protected function _init() {
+    protected $_allowEdit = false;
 
-		Aitsu_Content_Edit :: noEdit('Search.Lucene', true);
+    protected function _init() {
 
-		Aitsu_Registry :: setExpireTime(0);
+        Aitsu_Registry::setExpireTime(0);
 
-		$searchterm = $_REQUEST['searchterm'];
+        $searchterm = $_REQUEST['searchterm'];
 
-		$view = $this->_getView();
+        $search_area = Aitsu_Config::get('search.lucene.area');
 
-		$view->searchterm = $searchterm;
+        $search_array = array();
+        foreach ($search_area as $idcat) {
+            $search_array[] = $idcat;
+        }
 
-		$search_area = Aitsu_Config :: get('search.lucene.area');
+        $view = $this->_getView();
+        $view->searchterm = $searchterm;
 
-		$search_array = array ();
-		foreach ($search_area as $idcat) {
-			$search_array[] = $idcat;
-		}
+        try {
+            $view->results = Aitsu_Lucene_Index::find($searchterm, $search_array);
+        } catch (Exception $e) {
+            trigger_error($e->getMessage());
+            $view->results = array();
+        }
 
-		try {
-			$view->results = Aitsu_Lucene_Index :: find($searchterm, $search_array);
-		} catch (Exception $e) {
-			$view->results = array ();
-		}
-
-		$output = $view->render('index.phtml');
-
-		return $output;
-	}
+        return $view->render('index.phtml');
+    }
 
 }
