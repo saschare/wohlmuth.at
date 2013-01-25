6128c3dcf8553ef4c9a43c54f8f35c6ef65f60d7
Andreas Kummer <a.kummer@wdrei.ch>
2013-01-25 20:00:24 +0100

    Reformatted only.

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
 
-
 /**
-* @author Anreas Kummer, w3concepts AG
-* @copyright Copyright &copy; 2012, w3concepts AG
-*/
-
+ * @author Anreas Kummer, w3concepts AG
+ * @copyright Copyright &copy; 2013, w3concepts AG
+ */
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
 
 define('REQUEST_START', microtime(true));
-$request = array_merge($_GET, $_POST, array (
-	$_SERVER['HTTP_HOST']
-));
+$request = array_merge($_GET, $_POST, array(
+    $_SERVER['HTTP_HOST']
+        ));
 ksort($request);
 define('REQUEST_HASH', hash('md4', serialize($request)));
-unset ($request);
+unset($request);
 define('CACHE_PATH', APPLICATION_PATH . '/data/pagecache');
 
 require_once (realpath('./library/Aitsu/Bootstrap.php'));
@@ -23,11 +21,11 @@ $etag = hash('md4', $content);
 
 header("ETag: {$etag}");
 
-if (isset ($_SERVER['HTTP_IF_NONE_MATCH']) && $etag === $_SERVER['HTTP_IF_NONE_MATCH']) {
-	header("HTTP/1.1 304 Not Modified");
-	header("Connection: Close");
-	exit (0);
+if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $etag === $_SERVER['HTTP_IF_NONE_MATCH']) {
+    header("HTTP/1.1 304 Not Modified");
+    header("Connection: Close");
+    exit(0);
 }
 
 echo $content;
-exit (0);
+exit(0);
