cb1d1769f1b6d7e2e434401284a59f810ac7e00c
Christian Kehres <c.kehres@webtischlerei.de>
2013-02-04 14:58:37 +0100

    Version 1.4.3, Revision 3

--- library/Moraso/Persistence/View/Media.php
+++ library/Moraso/Persistence/View/Media.php
@@ -55,22 +55,27 @@ class Moraso_Persistence_View_Media {
                         '	media.extension, ' .
                         '	description.name, ' .
                         '	description.subline ' .
-                        'from _media media ' .
-                        'left join _media_description description on media.mediaid = description.mediaid and description.idlang = :idlang ' .
+                        'from ' .
+                        '   _media as media ' .
+                        'left join ' .
+                        '   _media_description as description on media.mediaid = description.mediaid and description.idlang = :idlang ' .
                         'where ' .
-                        '	(media.idart = :idart or media.idart is null)' .
-                        '	and media.deleted is null ' .
-                        '	and media.mediaid in (' .
-                        '		select ' .
-                        '			max(media.mediaid) ' .
-                        '		from _media media ' .
-                        '		where ' .
-                        '			(idart = :idart or idart is null)' .
-                        '		group by' .
-                        '			filename ' .
-                        '	) ' .
+                        '   (media.idart = :idart or media.idart is null)' .
+                        'and ' .
+                        '   media.deleted is null ' .
+                        'and ' .
+                        '   media.mediaid in ( ' .
+                        '       select ' .
+                        '           max(media.mediaid) ' .
+                        '       from ' .
+                        '           _media media ' .
+                        '	where ' .
+                        '           (idart = :idart or idart is null) ' .
+                        '	group by ' .
+                        '           filename ' .
+                        '   ) ' .
                         'order by ' .
-                        '	' . $orderBy, array(
+                        '   ' . $orderBy, array(
                     ':idart' => Aitsu_Registry :: get()->env->idart,
                     ':idlang' => Aitsu_Registry :: get()->env->idlang
                 ));
