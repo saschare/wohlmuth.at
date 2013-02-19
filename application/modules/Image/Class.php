4176773730836c3a037ec553d25812893e3f1fdf
Christian Kehres <c.kehres@webtischlerei.de>
2013-02-19 11:51:45 +0100

    Version 1.4.4, Revision 2

--- application/modules/Image/Class.php
+++ application/modules/Image/Class.php
@@ -6,30 +6,46 @@
  */
 class Module_Image_Class extends Aitsu_Module_Abstract {
 
-    protected function _getDefaults() {
+    protected function _getDefaults($params) {
 
+        $imageWidth = empty($params->imageWidth) ? 200 : $params->imageWidth;
+        $imageHeight = empty($params->imageHeight) ? 200 : $params->imageHeight;
+        $imageRender = empty($params->imageRender) ? 0 : $params->imageRender;
+        $imageFloat = empty($params->imageFloat) ? '' : $params->imageFloat;
+        $imageStyle = empty($params->imageStyle) ? '' : $params->imageStyle;
+        $template = empty($params->template) ? 'index' : $params->template;
+        $idart = empty($params->idart) ? Aitsu_Registry :: get()->env->idart : $params->idart;
+        
+        $configurable = array();
+        $configurable['width'] = (!isset($params->configurable->width) && empty($params->configurable->width)) ? false : ($params->configurable->width == 'true') ? true : false;
+        $configurable['height'] = (!isset($params->configurable->height) && empty($params->configurable->height)) ? false : ($params->configurable->height == 'true') ? true : false;
+        $configurable['render'] = (!isset($params->configurable->render) && empty($params->configurable->render)) ? false : ($params->configurable->render == 'true') ? true : false;
+        $configurable['float'] = (!isset($params->configurable->float) && empty($params->configurable->float)) ? false : ($params->configurable->float == 'true') ? true : false;
+        $configurable['style'] = (!isset($params->configurable->style) && empty($params->configurable->style)) ? false : ($params->configurable->style == 'true') ? true : false;
+        $configurable['template'] = (!isset($params->configurable->template) && empty($params->configurable->template)) ? false : ($params->configurable->template == 'true') ? true : false;
+        
         return array(
-            'imageWidth' => 200,
-            'imageHeight' => 200,
-            'imageRender' => 0,
-            'imageFloat' => '',
-            'imageStyle' => '',
-            'template' => 'index',
-            'idart' => Aitsu_Registry :: get()->env->idart,
+            'imageWidth' => $imageWidth,
+            'imageHeight' => $imageHeight,
+            'imageRender' => $imageRender,
+            'imageFloat' => $imageFloat,
+            'imageStyle' => $imageStyle,
+            'template' => $template,
+            'idart' => $idart,
             'configurable' => array(
-                'width' => true,
-                'height' => true,
-                'render' => true,
-                'float' => true,
-                'style' => true,
-                'template' => true
+                'width' => $configurable['width'],
+                'height' => $configurable['height'],
+                'render' => $configurable['render'],
+                'float' => $configurable['float'],
+                'style' => $configurable['style'],
+                'template' => $configurable['template']
             )
         );
     }
 
     protected function _main() {
 
-        $defaults = $this->_getDefaults();
+        $defaults = $this->_getDefaults($this->_params);
 
         $idart = empty($this->_params->idart) ? $defaults['idart'] : $this->_params->idart;
         $template = empty($this->_params->template) ? $defaults['template'] : $this->_params->template;
