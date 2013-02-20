10ee377d89579f5e60264b1145f7e8cf8f7f8273
Christian Kehres <c.kehres@webtischlerei.de>
2013-02-20 14:48:27 +0100

    Version 1.4.4, Revision 9

--- application/modules/HTML/Meta/CanonicalTag/Class.php
+++ application/modules/HTML/Meta/CanonicalTag/Class.php
@@ -1,44 +1,42 @@
 <?php
 
-
 /**
- * @author Andreas Kummer, w3concepts AG
- * @copyright Copyright &copy; 2011, w3concepts AG
+ * @author Christian Kehres <c.kehres@webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
  */
+class Skin_Module_HTML_Meta_CanonicalTag_Class extends Aitsu_Module_Abstract {
 
-class Module_HTML_Meta_CanonicalTag_Class extends Aitsu_Module_Abstract {
+    protected $_allowEdit = false;
 
-	protected $_allowEdit = false;
+    protected function _main() {
 
-	protected function _main() {
+        $art = Aitsu_Persistence_Article :: factory(Aitsu_Registry :: get()->env->idart, Aitsu_Registry :: get()->env->idlang)->load();
 
-		$art = Aitsu_Persistence_Article :: factory(Aitsu_Registry :: get()->env->idart, Aitsu_Registry :: get()->env->idlang)->load();
+        $base = substr(Aitsu_Config :: get('sys.webpath'), 0, -1);
+        $canonicalPath = Aitsu_Config :: get('sys.canonicalpath');
+        if ($canonicalPath != null) {
+            $base = substr(Aitsu_Config :: get('sys.canonicalpath'), 0, -1);
+        }
 
-		$base = substr(Aitsu_Config :: get('sys.webpath'), 0, -1);
-		$canonicalPath = Aitsu_Config :: get('sys.canonicalpath');
-		if ($canonicalPath != null) {
-			$base = substr(Aitsu_Config :: get('sys.canonicalpath'), 0, -1);
-		}
+        if ($art->idcat == Aitsu_Config::get('sys.startcat')) {
+            if (Aitsu_Config::get('rewrite.uselang')) {
+                $language = Aitsu_Persistence_Language::factory(Aitsu_Registry::get()->env->idlang)->name;
+                $href = '/' . $language . '/';
+            } else {
+                $href = '/';
+            }
+        } elseif ($art->startidartlang == $art->idartlang) {
+            $href = '{ref:idcat-' . $art->idcat . '}';
+        } else {
+            $href = '{ref:idart-' . $art->idart . '}';
+        }
 
-                if ($art->idcat == Aitsu_Config::get('sys.startcat')) {
-                    if (Aitsu_Config::get('rewrite.uselang')) {
-                        $language = Aitsu_Persistence_Language::factory(Aitsu_Registry::get()->env->idlang)->name;
-                        $href = '/' . $language . '/"';
-                    } else {
-                        $href = '/"';
-                    }
-                } elseif ($art->startidartlang == $art->idartlang) {
-			$href = '{ref:idcat-' . $art->idcat . '}"';
-		} else {
-			$href = '{ref:idart-' . $art->idart . '}"';
-		}
+        return '<link rel="canonical" href="' . $base . '' . $href . '" />';
+    }
 
-		return '<link rel="canonical" href="' . $base . '' . $href . '" />';
-	}
+    protected function _cachingPeriod() {
 
-	protected function _cachingPeriod() {
-
-		return 60 * 60 * 24 * 365;
-	}
+        return 'eternal';
+    }
 
 }
