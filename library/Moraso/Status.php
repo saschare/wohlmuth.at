ad139d242f0ccddd707f2b3351d36d19f476cd17
Christian Kehres <c.kehres@webtischlerei.de>
2013-02-26 12:12:23 +0100

    Version 1.4.6, Revision 25

--- library/Moraso/Status.php
+++ library/Moraso/Status.php
@@ -8,7 +8,7 @@ class Moraso_Status {
 
     public static function version() {
 
-        $version = '$version/1.4.6/revision/24$';
+        $version = '$version/1.4.6/revision/25$';
 
         return str_replace(array(
             'version/',
