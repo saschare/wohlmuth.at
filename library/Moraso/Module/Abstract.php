7f6820c14c63ea4a38957a01b840964df04e4293
Christian Kehres <c.kehres@webtischlerei.de>
2013-03-02 17:33:16 +0100

    initial commit

---
+++ library/Moraso/Module/Abstract.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author Christian Kehres <c.kehres@webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
+ */
+abstract class Moraso_Module_Abstract extends Aitsu_Module_Abstract {
+    
+}
