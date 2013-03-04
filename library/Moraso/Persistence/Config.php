d945d8432e00585d21c1c21544df2550cc08fe9d
Christian Kehres <c.kehres@webtischlerei.de>
2013-03-04 15:06:53 +0100

    Version 1.6.0, Revision 7

--- library/Moraso/Persistence/Config.php
+++ library/Moraso/Persistence/Config.php
@@ -4,16 +4,10 @@
  * @author Christian Kehres <c.kehres@webtischlerei.de>
  * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
  */
-class Moraso_Persistence_Config extends Aitsu_Persistence_Abstract {
-
-    protected $_idlang = null;
-    protected $_env = null;
-    protected $_idclient = null;
+class Moraso_Persistence_Config {
 
     protected function __construct($id) {
-
         $this->_id = $id;
-        $this->_idlang = Moraso_Util::getIdlang();
         
         $this->load();
     }
@@ -60,43 +54,18 @@ class Moraso_Persistence_Config extends Aitsu_Persistence_Abstract {
         return $this;
     }
 
-    public function __get($key) {
-
-        $keyParts = explode(':', $key);
-
-        $client = $keyParts[0];
-        $env = $keyParts[1];
-        $identifier = $keyParts[2];
-
-        if (!isset($this->_data[$client][$env][$identifier])) {
-            $this->_data[$client][$env][$identifier] = array();
-        }
-
-        return $this->_data[$client][$env][$identifier];
-    }
-
-    public function __set($key, $value) {
-
-        $keyParts = explode(':', $key);
-
-        $client = $keyParts[0];
-        $env = $keyParts[1];
-        $identifier = $keyParts[2];
+    public function setValue($client, $env, $identifier, $value) {
 
         $this->_data[$client][$env][$identifier] = $value;
     }
+    
+    public function unsetValue($client, $env, $identifier) {
 
-    public function __isset($key) {
-
-        $keyParts = explode(':', $key);
-
-        $client = $keyParts[0];
-        $env = $keyParts[1];
-        $identifier = $keyParts[2];
-
-        return isset($this->_data[$client][$env][$identifier]);
+        if (isset($this->data[$client][$env][$identifier])) {
+            unset($this->data[$client][$env][$identifier]);
+        }
     }
-
+    
     public function save() {
 
         if (empty($this->_data)) {
@@ -128,18 +97,6 @@ class Moraso_Persistence_Config extends Aitsu_Persistence_Abstract {
         }
 
         return $this;
-    }
-
-    public function unsetValue($client, $env, $identifier) {
-
-        if (isset($this->data[$client][$env][$identifier])) {
-            unset($this->data[$client][$env][$identifier]);
-        }
-    }
-
-    public function setValue($client, $env, $identifier, $value) {
-
-        $this->_data[$client][$env][$identifier] = $value;
     }
 
 }
