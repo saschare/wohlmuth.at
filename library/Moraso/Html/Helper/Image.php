5d0d91738e219f1319f98c9c022c08733e411055
Christian Kehres <c.kehres@webtischlerei.de>
2013-03-07 18:29:15 +0100

    Version 1.8.0, Revision 3

--- library/Moraso/Html/Helper/Image.php
+++ library/Moraso/Html/Helper/Image.php
@@ -6,41 +6,49 @@
  */
 class Moraso_Html_Helper_Image {
 
-    public static function getHtml($idart, $filename, $width, $height, $boxed, $attributes) {
+    public static function getHtml($idart, $filename, $width, $height, $boxed, $attributes = null) {
 
-        $attributes = (object) $attributes;
-        
-        $return = '<img src="' . self :: getPath($idart, $filename, $width, $height, $boxed) . '"';
+        $imgPath = self::getPath($idart, $filename, $width, $height, $boxed);
 
         if (!empty($attributes)) {
-            foreach ($attributes as $attr => $value) {
-                if (is_object($value)) {
-                    $return .= ' ' . $attr . '="';
-                    foreach ($value as $style => $styleValue) {
-                        if (!empty($styleValue)) {
-                            if ($style == 'self') {
-                                $return .= $styleValue;
-                            } else {
-                                $return .= '' . $style . ': ' . $styleValue . ';';
-                            }
+            $imgAttr = self::_createAttributes($attributes);
+        }
+
+        return '<img src="' . $imgPath . '" ' . $imgAttr . ' />';
+    }
+
+    public static function getPath($idart, $filename, $width, $height, $boxed) {
+
+        return Aitsu_Config::get('sys.webpath') . 'image/' . $width . '/' . $height . '/' . $boxed . '/' . $idart . '/' . $filename;
+    }
+
+    private static function _createAttributes($attributes) {
+
+        $attributes = (object) $attributes;
+
+        $return = '';
+
+        foreach ($attributes as $attr => $value) {
+            if (is_object($value)) {
+                $return .= ' ' . $attr . '="';
+                foreach ($value as $style => $styleValue) {
+                    if (!empty($styleValue)) {
+                        if ($style == 'self') {
+                            $return .= $styleValue;
+                        } else {
+                            $return .= '' . $style . ': ' . $styleValue . ';';
                         }
                     }
-                    $return .= '"';
-                } else {
-                    if (!empty($value)) {
-                        $return .= ' ' . $attr . '="' . $value . '"';
-                    }
+                }
+                $return .= '"';
+            } else {
+                if (!empty($value)) {
+                    $return .= ' ' . $attr . '="' . $value . '"';
                 }
             }
         }
-        $return .= ' />';
 
         return $return;
-    }
-    
-    public static function getPath($idart, $filename, $width, $height, $boxed) {
-        
-        return '/image/' . $width . '/' . $height . '/' . $boxed . '/' . $idart . '/' . $filename;
     }
 
 }
